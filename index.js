1be03b18e3a4092bc350ffe0c34fbb983649b570
sarkerrakib <freelancerrakib25@gmail.com>
2022-05-08 22:11:05 +0600

    product add button on server side

--- index.js
+++ index.js
@@ -47,7 +47,12 @@ async function run(){
         });
 
         // Delete
-        
+        app.delete('/store/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        });
         app.put('/delivered/:id',async(req,res)=>{
             const id = req.params.id;
             const updataquantity= req.body.quantity
